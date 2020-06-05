0a464116caa44fd03d7c759eb81044723ff1e84d
reme3d2y <AYatsenko@alfabank.ru>
2020-06-05 10:11:50 +0300

    refactor(*): save to one file

--- index.ts
+++ index.ts
@@ -8,33 +8,18 @@ const writeFile = promisify(fs.writeFile);
 
 (async () => {
   let allComponents = {};
-  let allTextStyles = {};
 
   for (let platform of Object.keys(libraries) as PLATFORM[]) {
-    const componentsResponses = await Promise.all<FigmaItem[]>(
-      libraries[platform]
-        .filter((library) => library.type !== 'typography')
-        .map((library) => requestComponents(library.file_key))
-    );
+    for (let library of libraries[platform]) {
+      const response =
+        library.type === 'typography'
+          ? await requestStyles(library.file_key)
+          : await requestComponents(library.file_key);
 
-    const textStylesResponses = await Promise.all<FigmaItem[]>(
-      libraries[platform]
-        .filter((library) => library.type === 'typography')
-        .map((library) => requestStyles(library.file_key))
-    );
-
-    componentsResponses.forEach((response) => {
       allComponents = { ...allComponents, ...buildJSONMeta(response, platform) };
-    });
-
-    textStylesResponses.forEach((response) => {
-      allTextStyles = { ...allTextStyles, ...buildJSONMeta(response, platform) };
-    });
+    }
   }
 
   const componentsFileName = path.resolve(__dirname, './data/components.json');
   await writeFile(componentsFileName, JSON.stringify(allComponents, null, 4), 'UTF-8');
-
-  const textStylesFileName = path.resolve(__dirname, './data/text-styles.json');
-  await writeFile(textStylesFileName, JSON.stringify(allTextStyles, null, 4), 'UTF-8');
 })();
