289c930dc3c1485807f7ba57f9d3e1da5a137db8
reme3d2y <AYatsenko@alfabank.ru>
2021-01-12 16:57:43 +0300

    feat: autolink, parse, filter

--- index.ts
+++ index.ts
@@ -3,8 +3,73 @@ import fs from 'fs';
 import { promisify } from 'util';
 import libraries from './data/libraries.json';
 import { requestComponents, requestStyles, downloadImage } from './utils';
+import axios from 'axios';
 
 const writeFile = promisify(fs.writeFile);
+
+const buildItem = (component: FigmaItem, libraryName: string, platform: Platform) => {
+    const isVariant = !!component.containing_frame?.containingStateGroup;
+
+    const item: ShortFigmaItem = {
+        key: component.key,
+        file_key: component.file_key,
+        library_name: libraryName,
+        name: isVariant ? component.containing_frame.containingStateGroup.name : component.name,
+        description: component.description,
+        platform,
+    };
+
+    if (isVariant) {
+        item.variant = component.name;
+    }
+
+    return item;
+};
+
+const filterItems = (component: ShortFigmaItem) => {
+    const name = component.name.toLocaleLowerCase().trim();
+
+    return name.startsWith('.') === false && name.includes('deprecated') === false;
+};
+
+const findPairs = async (components: ShortFigmaItem[]) => {
+    const r = await axios.get('https://digital.alfabank.ru/figma-pairs/pairs');
+    const keys = r.data.toString();
+
+    const byPlatform = {
+        IOS: components.filter((c) => c.platform === 'IOS'),
+        ANDROID: components.filter((c) => c.platform === 'ANDROID'),
+    };
+
+    const match = (c1: ShortFigmaItem, c2: ShortFigmaItem) => {
+        if (c1.variant && c2.variant) {
+            const variant1 = JSON.stringify(c1.variant.split(', ').sort());
+            const variant2 = JSON.stringify(c2.variant.split(', ').sort());
+            return c1.name === c2.name && variant1 === variant2;
+        }
+    };
+
+    const newPairs: Array<[ShortFigmaItem, ShortFigmaItem]> = [];
+
+    for (const ios of byPlatform.IOS) {
+        let matched = false;
+
+        for (const android of byPlatform.ANDROID) {
+            matched = match(ios, android);
+            if (matched && !keys.includes(ios.key) && !keys.includes(android.key)) {
+                newPairs.push([ios, android]);
+                break;
+            }
+        }
+
+        if (matched) continue;
+    }
+
+    for await (const [c1, c2] of newPairs) {
+        await axios.post('https://digital.alfabank.ru/figma-pairs/link', { key1: c1.key, key2: c2.key });
+        console.log(`[+] autolink: ${c1.name}:${c1.key} <-> ${c2.name}:${c2.key}`);
+    }
+};
 
 (async () => {
     const allComponents: ShortFigmaItem[] = [];
@@ -17,24 +82,9 @@ const writeFile = promisify(fs.writeFile);
                     : await requestComponents(library.file_key);
 
             allComponents.push(
-                ...response.map<ShortFigmaItem>((component) => {
-                    const isVariant = !!component.containing_frame?.containingStateGroup;
-
-                    const item: ShortFigmaItem = {
-                        key: component.key,
-                        file_key: component.file_key,
-                        library_name: library.name,
-                        name: isVariant ? component.containing_frame.containingStateGroup.name : component.name,
-                        description: component.description,
-                        platform,
-                    };
-
-                    if (isVariant) {
-                        item.variant = component.name;
-                    }
-
-                    return item;
-                })
+                ...response
+                    .map<ShortFigmaItem>((component) => buildItem(component, library.name, platform))
+                    .filter(filterItems)
             );
 
             console.log(`[+]${library.file_key} saved`);
@@ -59,4 +109,6 @@ const writeFile = promisify(fs.writeFile);
 
     const componentsFileName = path.resolve(__dirname, './data/components.json');
     await writeFile(componentsFileName, JSON.stringify(allComponents, null, 4), 'UTF-8');
+
+    await findPairs(allComponents);
 })();
